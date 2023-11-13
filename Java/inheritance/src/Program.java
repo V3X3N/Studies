0f063dd3ef40f621d1e5474e643e1efb3d990503
Marcin Kawa <lerukcja2002@gmail.com>
2023-11-13 21:41:18 +0100

    complete exercise

--- Java/inheritance/src/Program.java
+++ Java/inheritance/src/Program.java
@@ -1,24 +1,31 @@
-import java.awt.Point;
+import java.awt.*;
 
-class Program
+public class Program
 {
     public static void main(String[] args)
     {
+        Prostokat a=new Prostokat(3,4); //Tworzymy obiekt a klasy Prostokat
+        a.info(); //Wywolujemy metode info, ktora korzysta z toString z klasy Rectangle
 
-        Prostokat a=new Prostokat(3,4);
-        a.info();
 
-        Prostokat b=new Prostokat(2,2);
-        b.info();
+        Prostokat b=new Prostokat(2,2); //Tworzymy obiekt b klasy Prostokat
+        b.info(); //Wywolujemy metode info, ktora korzysta z toString z klasy Rectangle
 
-        Point wierzcholek = new Point(1, 2);
+
+        if (a.przylega(b)) {
+            System.out.println("-- przylega --\n");
+        } else {
+            System.out.println("-- NIE przylega --\n");
+        }
+
+
+        // Przykładowe użycie konstruktora
+        Point wierzcholek = new Point(1, 1);
         Prostokat c = new Prostokat(wierzcholek, 3, 4);
         c.info();
 
 
-
-
-        if(a.intersects(b))
+        if(a.intersects(b)) //Sprawdzamy czy przecinaja sie dzieki Rectangle. Metoda zwraca true/false w zaleznosci od przecinania sie
         {
             System.out.println("-- przecinaja sie --\n");
         }
@@ -28,11 +35,10 @@ class Program
         }
 
 
+        a.translate(5,3); //Wykorzystanie metody translate z klasy Rectangle
+        a.info(); //Wywolujemy metode info, ktora korzysta z toString z klasy Rectangle
 
-        a.translate(5,3);
-        a.info();
-
-        if(a.intersects(b))
+        if(a.intersects(b)) //Sprawdzamy czy przecinaja sie dzieki Rectangle. Metoda zwraca true/false w zaleznosci od przecinania sie
         {
             System.out.println("-- przecinaja sie --\n");
         }
@@ -40,6 +46,12 @@ class Program
         {
             System.out.println("-- NIE przecinaja sie --\n");
         }
-
     }
 }
+
+/*
+Tak, konstruktory nadklasy są dziedziczone przez podklasę.
+Kiedy tworzysz instancję podklasy, konstruktor nadklasy jest wywoływany automatycznie,
+aby zainicjować dziedziczone pola i dostosować stan obiektu.
+To jest realizowane poprzez wywołanie super() jako pierwszej instrukcji w konstruktorze podklasy
+ */
--- Java/inheritance/src/Prostokat.java
+++ Java/inheritance/src/Prostokat.java
@@ -1,12 +1,20 @@
-import java.awt.Rectangle;
-import java.awt.Point;
-class Prostokat extends Rectangle {
-    Prostokat(Point wierzcholek, int dlugosc, int szerokosc) {
-        super(wierzcholek.x, wierzcholek.y, dlugosc, szerokosc);
-    }
+import java.awt.*;
+
+class Prostokat extends Rectangle
+{
     Prostokat(int a,int b)
     {
         super(a,b);
+    }
+
+    Prostokat(Point wierzcholek, int dlugosc, int szerokosc)
+    {
+        //Wywołanie konstruktora nadklasy z argumentami
+        super(wierzcholek.x, wierzcholek.y, dlugosc, szerokosc);
+    }
+
+    boolean przylega(Prostokat innyProstokat) {
+        return (this.intersects(innyProstokat.x - 1, innyProstokat.y - 1, innyProstokat.width + 2, innyProstokat.height + 2));
     }
 
     void info()
